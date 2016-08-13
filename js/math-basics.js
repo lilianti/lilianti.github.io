8b2a08e3bd40c1d5a93adee5c79464ad77102876
lilianti <lili.wibiesono@gmail.com>
2016-08-13 12:24:46 +0700

    edit tugas 3 & 4

--- js/math-basics.js
+++ js/math-basics.js
@@ -3,10 +3,10 @@
   for (i = 0; i < b; i++) { 
     hasil = hasil * a;
 }
- 	return hasil;
+  return hasil;
 }
 
-	function akarKuadrat(a) {
+  function akarKuadrat(a) {
     var hasil = 0; 
     var counter = 0;
     while (hasil != a && hasil < a){
@@ -16,7 +16,15 @@
     if (hasil == a) {
       hasil = counter;}
     else {
-    	hasil = "Bukan Bilangan Kuadrat";    
+      hasil = "Bukan Bilangan Kuadrat";    
     }
       return hasil;
+  }
+  
+  function hipotenusa (a,b){
+     return  akarKuadrat (pangkatAngka(3,2) + pangkatAngka(4,2));
+  }
+
+  function luasLingkaran(r){  
+    return Math.round(Math.PI * pangkatAngka(r,2));
   }
