f89dff127b9ab2e7ad5fabc9feb6cc1bc2034b45
lilianti <lili.wibiesono@gmail.com>
2016-08-14 20:52:09 +0700

    edit main suit

--- js/mainSuit.js
+++ js/mainSuit.js
@@ -1,7 +1,6 @@
   function pilihanPengguna(){
     var pengguna = prompt("Pilih Salah Satu : Rock / Paper / Scissor");
     pengguna = pengguna.toUpperCase();
-  	document.getElementById("pengguna").innerHTML = "Pilihan Pengguna: " + pengguna;
     return pengguna;
   }
   
@@ -15,18 +14,21 @@
         pilihanLawan = "PAPER";
       } else
         pilihanLawan = "SCISSOR";
-    document.getElementById("lawan").innerHTML = "Pilihan Lawan: "  + " (" + randomPilihan.toPrecision(3) + ") " + pilihanLawan;
    return pilihanLawan; 
   }
   
   function bertarung (a,b){
     bertarung = "Seri";
- 		if (a == "ROCK" && b == "PAPER") {bertarung = "Anda Kalah!";}
- 		if (a == "ROCK" && b == "SCISSOR") {bertarung = "Anda Menang!";}
- 		if (a == "PAPER" && b == "ROCK") {bertarung = "Anda Menang!";}
- 		if (a == "PAPER" && b == "SCISSOR") {bertarung = "Anda Kalah!";}
- 		if (a == "SCISSOR" && b == "ROCK") {bertarung = "Anda Kalah!";}
- 		if (a == "SCISSOR" && b == "PAPER") {bertarung = "Anda Menang!";}
+    if (a == "ROCK" && b == "PAPER") {bertarung = "Anda Kalah!";}
+    if (a == "ROCK" && b == "SCISSOR") {bertarung = "Anda Menang!";}
+    if (a == "PAPER" && b == "ROCK") {bertarung = "Anda Menang!";}
+    if (a == "PAPER" && b == "SCISSOR") {bertarung = "Anda Kalah!";}
+    if (a == "SCISSOR" && b == "ROCK") {bertarung = "Anda Kalah!";}
+    if (a == "SCISSOR" && b == "PAPER") {bertarung = "Anda Menang!";}
+    
+    document.getElementById("pengguna").innerHTML = "Pilihan Anda: " + a;
+    document.getElementById("lawan").innerHTML = "Pilihan Lawan: "   + b;
+    document.getElementById("hasil").innerHTML = "Hasil: " + bertarung;
     return bertarung;
   }
   
@@ -35,7 +37,9 @@
     var pengguna = pilihanPengguna();
     var lawan = pilihanLawan();
     var hasil = bertarung(pengguna,lawan);
-       
-    document.getElementById("hasil").innerHTML = "Hasil: " + hasil;
+    
+    console.log(pengguna);
+    console.log(lawan);
+    console.log(hasil);     
   }
   
