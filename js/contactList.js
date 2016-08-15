adbaae41dd0c8c68f21cf5785f85e4f9a8fb4115
lilianti <lili.wibiesono@gmail.com>
2016-08-15 21:36:31 +0700

    revisi js

--- js/contactList.js
+++ js/contactList.js
@@ -1,3 +1,4 @@
+<script>
   
   var daftarKontak = new Array();
   var contact = {
@@ -9,14 +10,14 @@
   var program = {
     tambahKontak : function(nama, email, telepon){
       
- 		daftarKontak.push({nama: nama, email:email,telepon:telepon});
+    daftarKontak.push({nama: nama, email:email,telepon:telepon});
     document.getElementById("tampilkanHasil").innerHTML = "New Contact :<br>" + nama + "<br>" + email + "<br>" + telepon ;
     },
     
     lihatSemua : function(){
- 			var hasil = "";
+      var hasil = "";
  
-     	var i = 0;
+      var i = 0;
       
       for (i = 0; i < daftarKontak.length; i++) {
           hasil = hasil + daftarKontak[i].nama + " / " + daftarKontak[i].email + " / " + daftarKontak[i].telepon + "<br>";
@@ -26,8 +27,20 @@
       console.log(daftarKontak.length);
     },
     cariKontak : function(){
-      alert("Maaf, menyusul :D");
-      
+      var cariNama = prompt("Nama yang anda cari ?");      
+      var found = 0;
+      var i = 0;
+     
+      for (i = 0 ; i < daftarKontak.length && found == 0; i++){      
+        if (daftarKontak[i].nama == cariNama) {
+          found = 1;
+          document.getElementById("tampilkanHasil").innerHTML = "Data ditemukan :<br>" + daftarKontak[i].nama + "<br>" +  daftarKontak[i].email +"<br>"  + daftarKontak[i].telepon ;
+        }
+      }
+      if (found == 0) {
+          alert("Data tidak ditemukan!");
+      }     
+ 
     }
   };
  
@@ -36,7 +49,7 @@
   function addContact(){
     var nama = prompt("Masukan Nama Anda :");   
     var email = prompt("Masukan Email Anda :");
-		var telepon = prompt("Masukan Telepon Anda :");
+    var telepon = prompt("Masukan Telepon Anda :");
     
     console.log(nama);
     console.log(email);
@@ -45,3 +58,5 @@
     program.tambahKontak(nama, email, telepon);
  
   }
+  
+</script>
